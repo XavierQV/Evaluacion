49a00139895e57705c2e94e95f30eeb06fb47b02
Xavier Quilo <xavierquilo@gmail.com>
2021-07-26 21:22:46 -0500

    Agregar archivos de proyecto.

---
+++ Evaluacion/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Evaluacion.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Evaluacion.Areas.Identity.IdentityHostingStartup))]
+namespace Evaluacion.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Evaluacion/Controllers/SociosController.cs
@@ -0,0 +1,105 @@
+﻿using Evaluacion.Data;
+using Evaluacion.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Evaluacion.Controllers
+{
+    public class SociosController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public SociosController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        // GET: SocioController
+        public ActionResult Index()
+        {
+            List<SocioViewModel> socios = new List<SocioViewModel>();
+            socios = _applicationDbContext.Socios.Select(q => new SocioViewModel
+            {
+                Cedula=q.Cedula,
+                Nombre = q.Nombre,
+                Apellido = q.Apellido,
+                Direccion = q.Direccion,
+                Estado = q.Estado,                
+            }).ToList();
+
+            return View(socios);
+        }
+
+        // GET: SocioController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SocioController/Create
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
+
+        // POST: SocioController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SocioController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SocioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SocioController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SocioController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
